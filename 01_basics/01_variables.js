ffb51af786995d10550e3e3e7c8e2537e12bea3e
prernasharma867 <prernasharma4053@gmail.com>
2026-01-25 20:49:04 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "prerna@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountName;
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@gamil.com";
+accountPassword = "456788";
+accountCity = "Ranchi"
+
+//console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountName])
