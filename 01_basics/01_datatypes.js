43ed96592b039228fe1d6baa24284a22b15fa1d1
prernasharma867 <prernasharma4053@gmail.com>
2026-01-25 20:47:56 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//alert("hello")//we are using node.js not browser
+let name = "Rahul" // string
+let age = 45 // number
+let star = true // boolean 
+// symbol 
+console.log(typeof star)
