bc964c1aed5a534a67fecd8786151d948dee7532
prernasharma867 <prernasharma4053@gmail.com>
2026-01-26 09:56:30 +0000

    operations

---
+++ 01_basics/01_operation.js
@@ -0,0 +1,18 @@
+let a = 6;
+let b = 5;
+console.log(a+b);
+console.log(a/b);
+console.log(a%b);
+console.log(a*b);
+console.log(a**b);
+console.log(a-b);
+let str1 = "hello";
+let str2 = " world";
+console.log(str1+str2);
+console.log("1"+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(2+2+"4");
+ let gameCounter = 122;
+ gameCounter++; //postfix
+ console.log(gameCounter);
