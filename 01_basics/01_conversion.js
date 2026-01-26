9de118373f45276c42838c3a94c22c6d270943cb
prernasharma867 <prernasharma4053@gmail.com>
2026-01-26 09:26:28 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,24 @@
+let score = "44"
+console.log( typeof score);
+
+let convertnumber = Number(score);
+console.log( typeof convertnumber);
+// number can be converted into string
+
+let marks = 78
+
+let convertmarks = String(marks);
+console.log( typeof convertmarks);
+ 
+// number can be converted into string
+
+let color = "yellow";
+console.log(color);
+
+let convertcolor = Number(color);
+console.log(typeof convertcolor);
+console.log(convertcolor); 
+//string cannot be converted into number
+
+let email = "sahil@234";
+console.log(typeof email);
