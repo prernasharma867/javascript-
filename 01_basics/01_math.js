2dec3812113541de74d6d55161cd91ed5a85cbfb
prernasharma867 <prernasharma4053@gmail.com>
2026-01-28 14:35:59 +0000

    memory

---
+++ 01_basics/01_math.js
@@ -0,0 +1,13 @@
+const score = 100
+const balance = new Number(100)
+console.log(score);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+const otherNumber = 23.4556;
+console.log(balance.toPrecision(3));
+const hundreds = 10000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+//*********************maths */
